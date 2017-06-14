34a61a29861b06d85c84ce2d1fcd0892afa302df
Chris Tjon <ctjon@redhat.com>
2017-06-13 17:33:58 -0700

    Add-Type -AssemblyName "Microsoft.SqlServer.Management.SMO"

--- windows/wamp_haproxy/roles/mssql/files/create-db.ps1
+++ windows/wamp_haproxy/roles/mssql/files/create-db.ps1
@@ -1,7 +1,8 @@
 # Create the database
 set-psdebug -strict
 $error[0]|format-list -force
-[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
+#[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO') | out-null
+Add-Type -AssemblyName "Microsoft.SqlServer.Management.SMO"
 $srv = new-Object Microsoft.SqlServer.Management.Smo.Server("(local)")
 $db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, "Ansible Demo DB")
 $db.Create()
